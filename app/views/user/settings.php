fb12668dccd1630e72e72270a26735606bed75c6
clstialdev <rusty75001@gmail.com>
2024-01-19 15:20:40 +0100

    ui/updated-settings-page

--- app/views/user/settings.php
+++ app/views/user/settings.php
@@ -1,3 +1,12 @@
+<?php 
+require_once '../../helpers/session_helper.php';
+
+$tab = $_GET['tab'] ?? 'my-details'; // Default to 'my-details' if no tab is set
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -12,18 +21,43 @@
     <link rel="stylesheet" href="../../../public/css/global.css" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
   </head>
   <body>
+    <?php include_once '../header.php'; ?>
     <div class="" style="padding-left: 180px">
       <div class="container-fluid px-4 pt-5">
         <h1>Settings</h1>
         <!-- NAV -->
         <ul class="container-fluid d-flex list-unstyled gap-4 pt-2">
-          <li class="active-tab nav-tab">My details</li>
-          <li class="nav-tab">Security</li>
-          <li class="nav-tab">Account Access</li>
-          <li class="nav-tab">Notifications</li>
+          <?php
+          $tab = $_GET['tab'] ?? 'my-details';
+            generateTabLink($tab, 'my-details', 'My details');
+            generateTabLink($tab, 'security', 'Security');
+            generateTabLink($tab, 'account-access', 'Account Access');
+            generateTabLink($tab, 'notifications', 'Notifications');
+          ?>
         </ul>
+
+        <?php
+          switch ($tab) {
+            case 'my-details':
+              include_once '../components/user/profile/my-details.php';
+              break;
+            case 'security':
+              include_once '../components/user/profile/security.php';
+              break;
+            case 'account-access':
+              include_once '../components/user/profile/account-access.php';
+              break;
+            case 'notifications':
+              include_once '../components/user/profile/notifications.php';
+              break;
+            default:
+              include_once '../components/user/profile/my-details.php';
+              break;
+          }
+      ?>
       </div>
     </div>
   </body>
