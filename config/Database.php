6bd7ff113ebc44ff38ffcded46b79d114dc06420
clstialdev <rusty75001@gmail.com>
2024-01-20 08:22:11 +0100

    fix/AHMED-updated-db

--- config/Database.php
+++ config/Database.php
@@ -8,7 +8,7 @@
  */
 
 class Database {
-    private $host = 'localhost:3308';
+    private $host = 'localhost';
     private $user = 'root';
     private $pass = '';
     private $dbname = 'bd_nitru';
